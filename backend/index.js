91c494931d99acaa235cae555422270a7a3015f9
wckedwisdom0911 <shivamrawat091125@gmail.com>
2024-06-08 02:54:32 +0530

    v11

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ const gameRoutes = require('./routes/game.routes.js');
 
 const app=express()
 const corsOptions = {
-    origin: "http://localhost:3000" // frontend URI (ReactJS)
+    origin: "https://6663798f254c8b36e9c04487--musical-cactus-480578.netlify.app/" // frontend URI (ReactJS)
 }
 app.use(express.json());
 app.use(cors(corsOptions));app.use(express.json());
