9bda148faa7e0fe09f911901d835e0db35b7a509
wckedwisdom0911 <shivamrawat091125@gmail.com>
2024-06-08 16:06:28 +0530

    s

--- backend/index.js
+++ backend/index.js
@@ -10,31 +10,10 @@ const app=express()
 app.use(express.json());
 
 
-allowedOrigins=['https://sevenup7down-assignment-frontend.onrender.com',"http://localhost:3000"]
-  
-
-const corsOptions = {
-    origin: (origin, callback) => {
-      if (!origin) return callback(null, true); // Allow requests with no origin (like mobile apps, curl)
-      if (allowedOrigins.indexOf(origin) === -1) {
-        const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
-        return callback(new Error(msg), false);
-      }
-      return callback(null, true);
-    },
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    preflightContinue: false,
-    optionsSuccessStatus: 204
-  };
-  
-  // Use CORS with the specified options
-  app.use(cors(corsOptions));
+  app.use(cors());
   app.use(express.json());
   
-  // Handle preflight requests
-  app.options('*', cors(corsOptions));
-  
+
 
 app.use('/game', gameRoutes);
 const path = require("path");
