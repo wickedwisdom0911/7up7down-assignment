3c49290384e54fbea91ebe28129265d8486c85b5
wckedwisdom0911 <shivamrawat091125@gmail.com>
2024-06-08 11:27:32 +0530

    added  new fixes

--- backend/index.js
+++ backend/index.js
@@ -6,12 +6,9 @@ const gameRoutes = require('./routes/game.routes.js');
 
 
 const app=express()
-const corsOptions = {
-    origin:"https://66637ae6d370ac3b5b2b5666--sparkling-sprite-f9b1f5.netlify.app"
 
-}
 app.use(express.json());
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json());
 
 app.use('/game', gameRoutes);
